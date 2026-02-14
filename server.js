71b5f15eb60d458b739166c055e0ba4dd2ab114d
suja-sk <nanimaa.heart@gmail.com>
2026-02-14 16:44:04 +0530

    Read secret from file

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 http.createServer((req, res) => {
   const env = process.env.APP_ENV || "Not Set";
-  const secret = process.env.DB_PASSWORD || "Not Set";
+
+  let secret = "Not Set";
+  try {
+    secret = fs.readFileSync("/etc/secrets/DB_PASSWORD", "utf8");
+  } catch (err) {
+    secret = "Secret File Not Found";
+  }
 
   res.end(`ENV: ${env}\nSECRET: ${secret}`);
 }).listen(8080);
